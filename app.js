e3b8e1c88781fe7a9b831bb675b2ec490a013b05
kevinledev <kevinle.developer@gmail.com>
2022-05-04 12:56:32 -0700

    created mongodb and server

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
 
-const messagesRoute = require('./routes/messages')
-app.use('/', messagesRoute)
-app.set('view engine', 'pug')
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+
+
+//testing simple request
+app.get("/", (req, res) => res.send("Hello World"));
+
+// const messagesRoute = require('./routes/messages')
+// app.use('/', messagesRoute)
+// app.set('view engine', 'pug')
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`TMWSD is listening at http://localhost:${port}`)
